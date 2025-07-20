171623b8353b18ea757e43fb1d1be49c5d009787
FTNeves80 <ftneves80@gmail.com>
2025-07-20 00:43:03 -0300

    Refactor main.py to improve code readability by adding comments and removing unnecessary line breaks

--- app/main.py
+++ app/main.py
@@ -4,24 +4,25 @@ from google.cloud import storage
 from dotenv import load_dotenv
 import os
 
+# Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
-
 gcp_credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcp_credentials
 
+# Faz a requisição para a API da Câmara dos Deputados
 resp = requests.get("https://dadosabertos.camara.leg.br/api/v2/deputados")
 data = resp.json()
 df_data = pd.DataFrame(data["dados"])
 
+# Filtra as colunas desejadas
 df_data_filtered = df_data[["nome","email","siglaUf","siglaPartido"]]
-
 json_data = df_data_filtered.to_json(orient="records", force_ascii=False)
 
 # Nome do bucket e arquivo
 bucket_name = "gcs_python_test"
 destination_blob_name = "df_data_filtered.json"  # Nome no GCS
 
+# Função para fazer o upload do JSON para o bucket do GCS
 def upload_json_to_bucket(bucket_name, destination_blob_name, json_content):
     # Cria cliente de storage
     client = storage.Client()
