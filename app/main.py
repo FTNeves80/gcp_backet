22e120c56f592ccdc1c68f37c12cdf5bec25c824
FTNeves80 <ftneves80@gmail.com>
2025-07-19 21:16:20 -0300

    Add Jupyter notebook and main application for data processing and upload
    - Created a Jupyter notebook to fetch data from the Brazilian Chamber of Deputies API, process it, and save it as a JSON file.
    - Implemented a main Python script that performs the same data fetching and processing, and uploads the resulting JSON to Google Cloud Storage.
    - Added functionality to upload JSON content directly to a specified GCS bucket.
    - Included necessary imports and environment variable setup for Google Cloud credentials.

---
+++ app/main.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+from google.cloud import storage
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Dell\ProjetosPython\terraform.json"
+
+resp = requests.get("https://dadosabertos.camara.leg.br/api/v2/deputados")
+data = resp.json()
+df_data = pd.DataFrame(data["dados"])
+
+df_data_filtered = df_data[["nome","email","siglaUf","siglaPartido"]]
+
+json_data = df_data_filtered.to_json(orient="records", force_ascii=False)
+
+# Nome do bucket e arquivo
+bucket_name = "gcs_python_test"
+destination_blob_name = "df_data_filtered.json"  # Nome no GCS
+
+def upload_json_to_bucket(bucket_name, destination_blob_name, json_content):
+    # Cria cliente de storage
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Envia o arquivo
+    blob.upload_from_string(json_content, content_type="application/json")
+
+    print(f"Arquivo {destination_blob_name} enviado para gs://{bucket_name}/{destination_blob_name}")
+
+
+# Faz o upload
+upload_json_to_bucket(bucket_name, destination_blob_name, json_data)
